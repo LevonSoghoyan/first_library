38c174826b3149cf93a96609e2e316627cdd307f
Levon Soghoyan <levon.soghoyan@taurotech.com>
2026-03-31 20:48:02 +0400

    Fixed basic function bugs; there were some issues in tt_strchr and tt_strnstr.

--- src/levlib.c
+++ src/levlib.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <stddef.h>
 /* Function isAlpha checks whether a character is alphabetic */
-int tt_isalpha (int c) 
+int tt_isalpha(int c) 
 {
 	if ((c > 40 && c < 91) || (c > 97 && c < 123)) {
 		return 1;
@@ -10,7 +10,7 @@ int tt_isalpha (int c)
 	return 0;
 }
 /* Function isAlpha checks whether a character is printable */
-int tt_isprint (int c) 
+int tt_isprint(int c) 
 {
 	if(c > 31 && c < 126) {
 		return 1;
@@ -18,7 +18,7 @@ int tt_isprint (int c)
 	return 0;
 }
 /*Function for counting string length*/
-size_t tt_strlen (const char *pSrc) 
+size_t tt_strlen(const char *pSrc) 
 {
 	size_t size = 0;
 	while (pSrc[size] !=  '\0') {
@@ -27,21 +27,21 @@ size_t tt_strlen (const char *pSrc)
 	return size;
 }
 /* This function copies one string to another */
-char* tt_strcpy (char *pDest, const char *pSrc) 
+char* tt_strcpy(char *pDest, const char *pSrc) 
 {
 	int i = 0;
-	while (pSrc[i++]) {
+	while (pSrc[i]) {
 		pDest[i] = pSrc[i];
 		i++;
 	}
 	return pDest;
 }
 /* This function copies <size> characters from one string to another */
-size_t tt_strlcpy (char *pDest, const char *pSrc, size_t size) 
+size_t tt_strlcpy(char *pDest, const char *pSrc, size_t size) 
 {
 	
 	int i = 0;
-	while (pSrc[i++] && size ) {
+	while (pSrc[i] && size ) {
 		pDest[i] = pSrc[i];
 		i++;
 		size--;
@@ -49,47 +49,54 @@ size_t tt_strlcpy (char *pDest, const char *pSrc, size_t size)
 	return i;
 }
 /* */
-int tt_tolower (int c) 
+int tt_tolower(int c) 
 {
 	return c > 64 && c < 91 ? c + 32 : c;
 }
 /*  */
-char* tt_strchr (const char *pSrc, int c) 
+char* tt_strchr(const char *pSrc, int c) 
 {
-	while (*pSrc && (*pSrc-'0') != c) {
-		pSrc++;
+    int i = 0;
+	while (pSrc[i] && (pSrc[i]-'0') != c) {
+		i++;
 	}
-	if (*pSrc !=  '\0' || c ==  0) {
-		return (char *)pSrc;
+	if (pSrc[i] !=  '\0' || c ==  0) {
+		return (char *) (pSrc + i);
 	}
 	return NULL;
 }
-int tt_strcmp (const char *pSrc1, const char *pSrc2) 
+
+int tt_strcmp(const char *pSrc1, const char *pSrc2) 
 {
-	while (*pSrc1  && *pSrc1 == *pSrc2 ) {
-		pSrc1++;
-		pSrc2++;
+    int i = 0;
+	while (pSrc1[i]  && pSrc1[i] == pSrc2[i] ) {
+		i++;
 	}
-	return (*pSrc1-'0') - (*pSrc2-'0');
+	return (pSrc1[i] - '0') - (pSrc2[i] - '0');
 }
-int tt_atoi (const char *pSrc)
+
+int tt_atoi(const char *pSrc)
 {
-	while (*pSrc == ' ') {
-		pSrc++;
+    int i = 0;
+	while (pSrc[i] && pSrc[i] == ' ') {
+		i++;
 	}
-	int dest=0;
-	while (*pSrc <= '9' && *pSrc >= '0') {
+	int dest = 0;
+	while (pSrc[i] && pSrc[i] <= '9' && pSrc[i] >= '0') {
 		dest *= 10;
-		dest += (*pSrc-'0');
+		dest += (pSrc[i] - '0');
+        i++;
 	}
 	return dest;
 }
+
 char* tt_strnstr(const char *pBig, const char *pLittle, size_t len) 
 {
-	char *pTemp = malloc(tt_strlen(pLittle) * 4 + 4);
-	while (len <= tt_strlen(pLittle)) {
-		tt_strlcpy(pTemp, pLittle, tt_strlen(pLittle));
-		if (tt_strcmp(pTemp, pLittle) == 0) {
+    int i = 0;
+    char *pTemp = malloc(sizeof(pLittle));
+	while (pBig[i] && len <= tt_strlen(pLittle)) {
+		tt_strlcpy(pTemp, pBig, tt_strlen(pLittle));
+		if (tt_strcmp(pTemp, (char*)(pBig + i) == 0)) {
 			return (char *)pBig;
 		}
 		pBig++;
@@ -97,6 +104,7 @@ char* tt_strnstr(const char *pBig, const char *pLittle, size_t len)
 	}
 	return NULL;
 }
+
 void* tt_calloc(size_t count, size_t size) 
 {
 	char *pDest = malloc (count * size);
@@ -107,12 +115,14 @@ void* tt_calloc(size_t count, size_t size)
 	}
 	return pDest;
 }
-char* strdup(const char *pSrc)
+
+char* tt_strdup(const char *pSrc)
 {
 	char *pDest = malloc (tt_strlen(pSrc) * 4 + 4);
 	tt_strcpy(pDest, pSrc);
 	return pDest;
 }
+
 char **tt_split (char const *s, char c)
 {
 	int nextIndx = 0;
@@ -123,8 +133,8 @@ char **tt_split (char const *s, char c)
 		while (s[i] != c) {
 			i++;
 		}
-	i++;
-	strCount++;
+		i++;
+		strCount++;
 	}
 	char **pDest = malloc(strCount * 8);
 	i = 0;
@@ -132,42 +142,49 @@ char **tt_split (char const *s, char c)
 		if (s[i] != c) {
 			nextIndx = i;
 		}
-		int charCount =0;
+		int charCount = 0;
 		while (s[i] != c) {
 			i++;
 			charCount++;
 		}
 		i++;
 		if (charCount != 0) {
-			pDest[strIndx] = malloc (charCount*4);
+			pDest[strIndx] = malloc(charCount * 4);
 			tt_strlcpy(pDest[strIndx], s + nextIndx, charCount);
 			strIndx++;
 		}
 	}
 	return pDest;
 }
-void tt_putchar_fd (char c, int fd) 
+
+void tt_putchar_fd(char c, int fd) 
 {
-//	write (fd, data, tt_strlen(data));
+//write (fd, data, tt_strlen(data));
 }
-LinkedList *tt_lstnew (void *content)
+
+LinkedList *tt_lstnew(void *content)
 {
 	LinkedList *pNode = malloc(9);
 	pNode -> content = content;
 	pNode -> next = NULL;
 	return pNode;
 }
+
 int tt_lstsize (LinkedList *lst)
 {
 	if (lst -> next == NULL)
 		return 1;
 	return 1 + tt_lstsize(lst++);
 }
+
+/*
+*/
 void  tt_lstadd_front(LinkedList **lst, LinkedList *new)
 {
 	new -> next = *lst;
 	*lst = new;
 }
+
 void  tt_lstadd_back(LinkedList **lst, LinkedList *new)
 {
 	LinkedList *pTemp = *lst;
