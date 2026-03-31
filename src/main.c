43dff9919b06767c39e7767a96787210f1d54cb3
Levon Soghoyan <levon.soghoyan@taurotech.com>
2026-03-31 20:48:02 +0400

    Fixed basic function bugs; there were some issues in tt_strchr and tt_strnstr.

--- src/main.c
+++ src/main.c
@@ -15,6 +15,14 @@ int main()
 	*pStrcpy = malloc(100);
 	tt_strlcpy(pStrcpy, pTemp, 5);
 	printf("%s\n", pStrcpy);
-	printf("%s\n", tt_tolower('H'));
+	printf("%c\n", tt_tolower('H'));
+ //   printf("%c\n", *tt_strchr(pTemp, 'o'));
+    printf("%d\n", tt_strcmp("gggc","gggg"));
+    printf("%d\n", tt_atoi("46548"));
+    //printf("%s\n", tt_strnstr("hello","llo", 6));   
+    printf("%d\n", tt_calloc(10,10));
+    printf("%s\n", tt_strdup("string"));
+    printf("%s\n", tt_split("my test string",' ')[0]);
+    
 	return 0;
 }
