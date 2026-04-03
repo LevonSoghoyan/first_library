acc36daa8eb2e196014c47f92085e5841261e6fb
Levon Soghoyan <levon.soghoyan@taurotech.com>
2026-04-03 15:47:45 +0400

    Added test.h file implementation.
    Added test functions for basic and additional functions.

--- src/main.c
+++ src/main.c
@@ -2,27 +2,27 @@
 #include <stdio.h>
 int main() 
 {
-	printf("%d\n", tt_isalpha(5));
+	printf("%d isalpha\n", tt_isalpha(5));
 	printf("%d\n", tt_isalpha('s'));
-	printf("%d\n", tt_isprint('a'));
+	printf("%d isprint\n", tt_isprint('a'));
 	printf("%d\n", tt_isprint(''));
 	const char *pTemp = "Hello";
-	printf("%d\n", tt_strlen(pTemp));
+	printf("%d strlen\n", tt_strlen(pTemp));
 	char *pStrcpy = malloc(100);
 	tt_strcpy(pStrcpy, pTemp);
-	printf("%s\n", pStrcpy);
+	printf("%sstrcpt\n", pStrcpy);
 	free(pStrcpy);
 	*pStrcpy = malloc(100);
 	tt_strlcpy(pStrcpy, pTemp, 5);
-	printf("%s\n", pStrcpy);
-	printf("%c\n", tt_tolower('H'));
- //   printf("%c\n", *tt_strchr(pTemp, 'o'));
-    printf("%d\n", tt_strcmp("gggc","gggg"));
-    printf("%d\n", tt_atoi("46548"));
-    //printf("%s\n", tt_strnstr("hello","llo", 6));   
-    printf("%d\n", tt_calloc(10,10));
-    printf("%s\n", tt_strdup("string"));
-    printf("%s\n", tt_split("my test string",' ')[0]);
+	printf("%s strlcpi\n", pStrcpy);
+	printf("%c tolower\n", tt_tolower('H'));
+    printf("%c strchr\n", *tt_strchr(pTemp, 'o'));
+    printf("%d strcmp\n", tt_strcmp("gggc","gggg"));
+    printf("%d atoi\n", tt_atoi("46548"));
+    printf("%s strnstr\n", tt_strnstr("hello","llo", 6));   
+    printf("%d calloc\n", tt_calloc(10,10));
+    printf("%s strdup\n", tt_strdup("string"));
+    printf("%s split\n", tt_split("my test string",' ')[1]);
     
 	return 0;
 }
